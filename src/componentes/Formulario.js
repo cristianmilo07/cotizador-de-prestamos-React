ba6a6db7adc0733dcba14aac69b197307ffb6ef6
Cristian Villada <cristianmilo07@gmail.com>
2020-04-12 16:43:23 -0500

    cuando el usuario hace submit, validar,mostrar error (si lo organiza no mostrar)

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -1,41 +1,65 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 const Formulario = ({cantidad, guardarCantidad, plazo, guardarPlazo}) => {
+
+    //Definir state
+    const [error, guardarError] = useState(false)
+
+    //cuando el usuario hace submit
+    const calcularPrestamo = e => {
+        e.preventDefault();
+        // console.log('Enviando formulario...')
+
+        // validar 
+        if( cantidad === 0  || plazo === ''){
+            guardarError(true);
+            return;
+        }
+
+        //Eliminar el error previo
+        guardarError(false);
+        
+        //Realizar la cotización
+    }
+
     return ( 
-        <form>
-          <div className="row">
-              <div>
-                  <label>Cantidad Prestamo</label>
-                  <input 
-                      className="u-full-width" 
-                      type="number" 
-                      placeholder="Ejemplo: 3000" 
-                      onChange = { (e) => {guardarCantidad (parseInt (e.target.value))}}
-                  />
-              </div>
-              <div>
-                  <label>Plazo para Pagar</label>
-                  <select 
-                      onChange = { (e) => {guardarPlazo (parseInt (e.target.value))}}
-                      className="u-full-width"
-                  >
-                      <option value="">Seleccionar</option>
-                      <option value="3">3 meses</option>
-                      <option value="6">6 meses</option>
-                      <option value="12">12 meses</option>
-                      <option value="24">24 meses</option>
-                      
-                  </select>
-              </div>
-              <div>
-                  <input 
-                      type="submit" 
-                      value="Calcular" 
-                      className="button-primary u-full-width" 
-                  />
-              </div>
-          </div>
-  </form>
-     );
+        <Fragment>
+            <form onSubmit={calcularPrestamo}>
+            <div className="row">
+                <div>
+                    <label>Cantidad Prestamo</label>
+                    <input 
+                        className="u-full-width" 
+                        type="number" 
+                        placeholder="Ejemplo: 3000" 
+                        onChange = { (e) => {guardarCantidad (parseInt (e.target.value))}}
+                    />
+                </div>
+                <div>
+                    <label>Plazo para Pagar</label>
+                    <select 
+                        onChange = { (e) => {guardarPlazo (parseInt (e.target.value))}}
+                        className="u-full-width"
+                    >
+                        <option value="">Seleccionar</option>
+                        <option value="3">3 meses</option>
+                        <option value="6">6 meses</option>
+                        <option value="12">12 meses</option>
+                        <option value="24">24 meses</option>
+                        
+                    </select>
+                </div>
+                <div>
+                    <input 
+                        type="submit" 
+                        value="Calcular" 
+                        className="button-primary u-full-width" 
+                    />
+                </div>
+            </div>
+            </form>
+            { (error) ? <p className="error"> Todos los campos son obligatorios</p> : ''}
+        </Fragment>
+    );
 }
  
 export default Formulario;
