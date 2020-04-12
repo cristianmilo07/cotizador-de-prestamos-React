dae2cf6cbfeb67e7995642049270371a0b3fd0f6
Cristian Villada <cristianmilo07@gmail.com>
2020-04-12 16:27:31 -0500

    Realizar función para obtener los valores de cantidad de prestamo y plazo a pagar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './componentes/Header'
+import Formulario from './componentes/Formulario'
 
 function App() {
+  //Definir el state
+  const [cantidad, guardarCantidad] = useState(0);
+  const [plazo, guardarPlazo] = useState(0);
   return (
     <Fragment>
       <Header
         titulo ="Cotizador de Prestamos"
       />
+      <div className="container">
+        <Formulario
+          cantidad={cantidad}
+          guardarCantidad={guardarCantidad}
+          plazo={plazo}
+          guardarPlazo={guardarPlazo}
+        />
+      </div>
     </Fragment>
   );
 }
